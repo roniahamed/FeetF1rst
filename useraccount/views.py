3230af657e1d31790e7bb4b6e9029edae96c04e7
roniahamed <mdroniahamed56@gmail.com>
2025-08-29 02:03:32 +0600

    Add email verification in user views
    - Implemented email verification logic in user views

--- useraccount/views.py
+++ useraccount/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.views import ApiView
+from rest_framework.response import Response 
+from .models import OTP, User 
+from rest_framework import status
+from .serializers import OTPSerializer, CustomRegisterSerializer
+from django.utils import timezone
+from django.conf import settings
+from django.core.mail import send_mail
+import random
 
-# Create your views here.
+class VerifyEmailOTPView(ApiView):
+    def post(self, request):
+        email = request.data.get('email')
+        otp_code = request.data.get('otp')
+        user = None
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return Response({"error": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+        
+        if user.is_verified:
+            return Response({"error": "User already verified."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        otp = OTP.objects.filter(user = user, code = otp_code, is_used=False, purpose= 'email_verification').last()
+
+        if not otp or otp.is_expired():
+            return Response({"error": "Invalid or expired OTP."}, status=status.HTTP_400_BAD_REQUEST)
+        otp.is_used = True
+        otp.save()
+        user.is_verified = True
+        user.save()
+        return Response({"message": "Email verified successfully."}, status=status.HTTP_200_OK)
